0ad7565bf626634b18484748da5c89b33ed0c2ae
Shivam Kumar <93670358+Shivamkr02@users.noreply.github.com>
2024-06-06 19:08:27 +0530

    Add files via upload
    This assignment solution is modified version of the assignment 10 . In this assignment solution we modify the above code so that now your table rows are displayed in alternate colors.
    
    We used CSS to set the alternate colors of the table rows

---
+++ Assignment11/app.js
@@ -0,0 +1,9 @@
+let a =Number(prompt("Enter the integer"));
+
+document.write(`<h3>Table of ${a}</h3>`)
+document.write(`<table border='2'>`);
+for(let i=1;i<=10;i++){
+    let rowClass = i % 2 === 0 ? 'dark-row' : 'light-row';
+    document.write(`<tr class="${rowClass}"><td>${i} * ${a} = ${i * a}</td></tr>`);
+}
+document.write(`</table`);
