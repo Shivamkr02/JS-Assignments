1f7b329ecc511b55c6726abfa50228cbe1f4d4d1
Shivam Kumar <93670358+Shivamkr02@users.noreply.github.com>
2024-06-06 23:25:40 +0530

    Add files via upload
    Write a Javascript code to accept an integer from the user and calculate it's factorial.

---
+++ Assignment12/app.js
@@ -0,0 +1,18 @@
+let number = Number(prompt("Enter a number to find its factorial:"));
+
+
+if (Number.isInteger(number) && number >= 0) {
+   
+    let result = 1;
+
+
+    for (let i = 1; i <= number; i++) {
+        result *= i;
+    }
+
+   
+    document.write(`<p>Factorial of ${number} is ${result}</p>`);
+} else {
+
+    document.write(`<p>Please enter a non-negative integer.</p>`);
+}
