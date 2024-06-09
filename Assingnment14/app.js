4ff4662a1f4d75fbdabaa230820fd27b5bdb9efa
Shivam Kumar <93670358+Shivamkr02@users.noreply.github.com>
2024-06-09 22:47:36 +0530

    Add files via upload
    Write JavaScript code to accept an integer from the user and calculate and print the sum of even and sum of odd nos up to given number seperately.

---
+++ Assingnment14/app.js
@@ -0,0 +1,15 @@
+let a = Number(prompt("Enter the integer"));
+
+let sumOfEven=0;
+
+let sumOfOdd =0;
+for(let i=1;i<=a;i++){
+    if(i%2 === 0){
+        sumOfEven += i;
+    }
+    else{
+        sumOfOdd += i;
+    }
+}
+document.write(`Sum of Even number is : ${sumOfEven}</br>`);
+document.write(`Sum of Odd number is : ${sumOfOdd}</br>`);
