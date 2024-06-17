3c51c3e13a1b08ab4ce173c41241a47176351a2a
Shivam Kumar <93670358+Shivamkr02@users.noreply.github.com>
2024-06-17 10:49:53 +0530

    Add files via upload
    Write a JavaScript to remove 0,false,undefined,null,NaN," from the given array.
    
    sample data
    
    input Array:  [NaN,0,15,false,-22,",undefined,47,null,94]
    
    output : [15,-22,47,94]

---
+++ Assignment21/app.js
@@ -0,0 +1,13 @@
+let inputArray = [NaN,0,15,false,-22,"",undefined,47,null,94];
+
+let outputArray = [];
+
+for(let i=0;i<inputArray.length;i++){
+    let value = inputArray[i];
+
+    if(value !== 0 && value !== false && value !== undefined && value !== null && !Number.isNaN(value) && value !== ""){
+        outputArray.push(value);
+    }
+}
+
+document.write(`Output Array: ${outputArray}`);
