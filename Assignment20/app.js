f30ec11d819c0ca427a68e90db66e093132530d5
Shivam Kumar <93670358+Shivamkr02@users.noreply.github.com>
2024-06-17 10:32:36 +0530

    Add files via upload
    Write a JavaScript code to remove all the duplicate elements from an array.

---
+++ Assignment20/app.js
@@ -0,0 +1,20 @@
+let numbers = [1,2,3,4,3,2,5,6,1,8,7,5];
+
+let uniqueNumbers = [];
+
+for(let i=0;i<numbers.length;i++){
+
+    let isDuplicate = false;
+    for(let j=0;j<uniqueNumbers.length;j++){
+        if(numbers[i]===uniqueNumbers[j]){
+            isDuplicate = true;
+            break;
+        }
+    }
+
+    if(!isDuplicate){
+        uniqueNumbers.push(numbers[i]);
+    }
+}
+
+document.write(`${uniqueNumbers}`);
