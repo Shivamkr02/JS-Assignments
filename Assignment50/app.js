cb7b0d24bc29ad05a039a1614d918b594692fad3
Shivam Kumar <93670358+Shivamkr02@users.noreply.github.com>
2024-07-02 10:03:58 +0530

    Add files via upload
    Write JavaScript functions decorate() and undecorate() to add border padding and to change the text color of paragraph with two button Click to add style and click to remove style .

---
+++ Assignment50/app.js
@@ -0,0 +1,9 @@
+function decorate(){
+    let para = document.querySelector("p");
+    para.classList.add("error");
+}
+
+function undecorate(){
+    let para = document.querySelector("p");
+    para.classList.remove("error");
+}
