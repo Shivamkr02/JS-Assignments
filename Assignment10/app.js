d8b2664b621028a0a476ada5015d1b28d43a30c1
Shivam Kumar <93670358+Shivamkr02@users.noreply.github.com>
2024-07-04 23:10:50 +0530

    Update app.js

--- Assignment10/app.js
+++ Assignment10/app.js
@@ -3,7 +3,7 @@ let a =Number(prompt("Enter the integer"));
 document.write(`<h3>Table of ${a}</h3>`)
 document.write(`<table border='2'>`);
 for(let i=1;i<=10;i++){
-    // document.write(`${i} * ${num} = ${i*num}</br>`);
+    // document.write(`${i} * ${num} = ${i*num}</br> `);
     document.write(`<tr><td>${i} * ${a} = ${i*a}</td></tr>`);
 }
 document.write(`</table`);
