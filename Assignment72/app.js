abb4a2cd8d2a8cd796bebc835b66630b5ddb03ee
Shivam Kumar <93670358+Shivamkr02@users.noreply.github.com>
2024-07-08 23:48:35 +0530

    Add files via upload
    Write an HTML page which accepts a phone Number from the user in the text box and does not allows anything else to be typed other than digits.

---
+++ Assignment72/app.js
@@ -0,0 +1,8 @@
+const x = document.getElementById("mytext");
+
+x.onkeypress = (e) => {
+    if(e.charCode < 48 || e.charCode > 57){
+        alert("Please input digits only");
+        return false;
+    }
+};
