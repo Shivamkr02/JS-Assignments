f7d601e2859140bdffcbeead8a3bade1cb1dc819
Shivam Kumar <93670358+Shivamkr02@users.noreply.github.com>
2024-07-11 20:30:03 +0530

    Add files via upload

---
+++ Assignment81/app.js
@@ -0,0 +1,31 @@
+const myform  = document.frmlogin;
+const txtusername = myform.txtuser;
+const txtpassword = myform.txtpwd;
+const spnunameerror = document.getElementById("unameerror");
+const spnpwderror = document.getElementById("pwderror");
+spnunameerror.innerText = spnpwderror.innerText = "";4
+myform.addEventListener("submit",(e) =>{
+    const user = txtusername.value;
+    const pwd = txtpassword.value;
+    let isvalid = true;
+    if(user.length == 0){
+        spnunameerror.innerText = "Username cannot be blanl!";
+        spnunameerror.style.color = "crimson";
+        txtusername.setAttribute("class","error");
+        isvalid = false;
+    } else{
+        spnunameerror.innerText = "";
+        txtusername.removeAttribute("class");
+    }
+    if(pwd.length == 0){
+        spnpwderror.innerText = "Password cannot be blank!";
+        spnpwderror.style.color = "crimson";
+        txtpassword.setAttribute("class","error");
+        isvalid = false;
+    } else{
+        spnpwderror.innerText = "";
+        txtpassword.removeAttribute("class");
+    }
+    if(!isvalid)
+        e.preventDefault();
+});
