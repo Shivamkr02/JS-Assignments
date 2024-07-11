50e09b07b6b10684c2548e45890dcadbc5bb38e9
Shivam Kumar <93670358+Shivamkr02@users.noreply.github.com>
2024-07-11 19:56:42 +0530

    Add files via upload

---
+++ Assignment80/app.js
@@ -0,0 +1,48 @@
+const myform = document.myfrm;
+const btnadd = myform.btnadd;
+const btnclear = myform.btnclear;
+const txtfno = myform.txtfno;
+const txtsno = myform.txtsno;
+const txtresult = myform.txtresult;
+const spnfnoerror = document.getElementById("fnoerror");
+const spnsnoerror = document.getElementById("snoerror");
+
+function validate(fnum,snum){
+    let isvalid = true;
+    if(fnum.length == 0 || isNaN(fnum)){
+        if(fnum.length == 0)
+            spnfnoerror.innerText = "Input Required!";
+        else
+            spnfnoerror.innerText = "Please input numbers only!"
+        spnfnoerror.style.color = "crimson";
+        txtfno.setAttribute("class","error");
+        isvalid = false;
+    }else{
+        spnfnoerror.innerText = "";
+        txtfno.removeAttribute("class");
+    }
+    if(snum.length == 0 || isNaN(snum)){
+        if(snum.length == 0)
+            spnsnoerror.innerText = "Input required";
+        else
+            spnsnoerror.style.innerText = "Please input numbers only!"
+        spnsnoerror.style.color = "crimson";
+        txtsno.setAttribute("class","error");
+        isvalid = false;
+    } else{
+        spnsnoerror.innerText = "";
+        txtsno.removeAttribute("class");
+    }
+    return isvalid;
+}
+btnadd.addEventListener("click",() => {
+    let fnum = txtfno.value;
+    let snum = txtsno.value;
+    if(validate(fnum,snum)){
+        let sum = Number(fnum) + Number(snum);
+        txtresult.value = sum;
+    }
+});
+btnclear.addEventListener('click', ()=>{
+    txtfno.value = txtsno.value = result.value = "";
+});
