294eaf9b37e825ca3e74193217e0ea64a558450c
Shivam Kumar <93670358+Shivamkr02@users.noreply.github.com>
2024-10-25 23:17:20 +0530

    Update app.js

--- Assignment20/app.js
+++ Assignment20/app.js
@@ -1,9 +1,6 @@
 let numbers = [1,2,3,4,3,2,5,6,1,8,7,5];
-
 let uniqueNumbers = [];
-
 for(let i=0;i<numbers.length;i++){
-
     let isDuplicate = false;
     for(let j=0;j<uniqueNumbers.length;j++){
         if(numbers[i]===uniqueNumbers[j]){
@@ -11,10 +8,8 @@ for(let i=0;i<numbers.length;i++){
             break;
         }
     }
-
     if(!isDuplicate){
         uniqueNumbers.push(numbers[i]);
     }
 }
-
 document.write(`${uniqueNumbers}`);
