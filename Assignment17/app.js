141d71e326a458c159cb5991a46dcaea450bb52b
Shivam Kumar <93670358+Shivamkr02@users.noreply.github.com>
2024-11-23 23:24:48 +0530

    Update app.js

--- Assignment17/app.js
+++ Assignment17/app.js
@@ -1,26 +1,17 @@
-
 let input = prompt("Enter an integer:");
 let num = parseInt(input);
-
-
 if (isNaN(num) || num <= 0) {
     console.log("Please enter a valid positive integer.");
-} else {
-    
+} else {  
     let primes = [];
-
     for (let i = 2; i <= num; i++) {
         let isPrime = true;
-
-       
         for (let j = 2; j * j <= i; j++) {
             if (i % j === 0) {
                 isPrime = false;
                 break;
             }
         }
-
-        
         if (isPrime) {
             primes.push(i);
         }
