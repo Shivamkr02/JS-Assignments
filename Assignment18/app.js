61628a06bdc2d15d4b31ef1ae09dc91d259ce364
Shivam Kumar <93670358+Shivamkr02@users.noreply.github.com>
2024-11-26 22:42:20 +0530

    Update app.js

--- Assignment18/app.js
+++ Assignment18/app.js
@@ -1,17 +1,21 @@
 let names = [];
-for (let i = 0; i < 5; i++) {
+for (let i = 0; i < 5; i++)
+    {
     let name = prompt("Enter a name:");
     names.push(name);
 }
 let searchName = prompt("Enter a name to search for:");
 let indices = [];
 let idx = names.indexOf(searchName);
-while (idx !== -1) {
+while (idx !== -1) 
+{
     indices.push(idx);
     idx = names.indexOf(searchName, idx + 1);
 }
-if (indices.length > 0) {
+if (indices.length > 0) 
+{
     document.write(`The name "${searchName}" was found at the following positions: ${indices.join(", ")}`);
-} else {
+} else 
+{
     document.write(`The name "${searchName}" was not found.`);
 }
