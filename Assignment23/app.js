5942d5d6a9da6eb729dcf0a73848ce155f7fb943
Shivam Kumar <93670358+Shivamkr02@users.noreply.github.com>
2024-12-11 23:03:39 +0530

    Update app.js

--- Assignment23/app.js
+++ Assignment23/app.js
@@ -5,12 +5,10 @@ function average(){
     for(let i=0;i<count;i++){
         sum+=arguments[i];
     }
-
     let avg = sum/count;
 
     return {sum,avg};
 }
-
 let result = average(2,4,6,8,10);
 document.write(`sum: ${result.sum}<br>`);
 document.write(`Average: ${result.avg}`);
