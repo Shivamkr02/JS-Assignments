577a0d5ad7713e1a5372c3f1935c337b5691bc62
Shivam Kumar <93670358+Shivamkr02@users.noreply.github.com>
2025-01-18 23:31:03 +0530

    Update app.js

--- Assignment12/app.js
+++ Assignment12/app.js
@@ -4,8 +4,6 @@ let number = Number(prompt("Enter a number to find its factorial:"));
 if (Number.isInteger(number) && number >= 0) {
    
     let result = 1;
-
-
     for (let i = 1; i <= number; i++) {
         result *= i;
     }
